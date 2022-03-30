6e590c2cf0985fa435885066f5e4507f3494bd13
Felipe <alu0101315713@ull.edu.es>
2022-03-30 13:22:26 +0100

    message

---
+++ p7/src/index.ts
@@ -0,0 +1,18 @@
+import {Artist} from '../src/Definitivos-Jerarquia/Principal-Clases/artist';
+import {Group} from '../src/Definitivos-Jerarquia/Principal-Clases/groups';
+import {MusicGenre} from '../src/Definitivos-Jerarquia/Principal-Clases/musicGenre';
+import {Song} from '../src/Definitivos-Jerarquia/Principal-Clases/song';
+import {Album} from '../src/Definitivos-Jerarquia/Principal-Clases/album';
+import {ArtistsCollection} from '../src/Definitivos-Jerarquia/Collectionables/artistCollection';
+
+const BadBunny = new Artist('BadBunny', 'Iluminati', 'Reggae', 'Touralmundo', '2', 12);
+const Bruno = new Artist('Bruno', 'Iluminati', 'Reggae', 'Touralmundo', '2', 12);
+const Iluminati = new Group('Iluminati', BadBunny, 2016, 'Reggae', 'Touralmundo', 12);
+const Trap = new MusicGenre('Trap', Iluminati, BadBunny, 'Touralmundo', 'Chambea');
+const Reggae = new MusicGenre('Reggae', Iluminati, BadBunny, 'Touralmundo', 'Chambea');
+const Chambea = new Song('Chambea', BadBunny, 5, Trap, '2', 2016015);
+const Touralmundo = new Album('Touralmundo', Iluminati, BadBunny, 2016, Trap, Chambea);
+const artistCollection = new ArtistsCollection([BadBunny, Bruno]);
+
+console.log(artistCollection.getName('Bruno'));
+console.log(artistCollection.getName('BadBunny'));
